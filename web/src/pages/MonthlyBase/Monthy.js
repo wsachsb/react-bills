eb6707d594a96e3cfd9ef027aaf1fcda7411a050
willian.sanches <wsb0101@gmail.com>
2024-02-06 17:10:04 -0300

    Created Monthly Funcition and Api get all list Month

--- web/src/pages/MonthlyBase/Monthy.js
+++ web/src/pages/MonthlyBase/Monthy.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useParams, Link } from "react-router-dom";
+import api from "../../services/api";
 
 const MontlyBaseGetMontly = () => {
 
     const { id } = useParams();
+
+    useEffect(() => {
+        api.get('/summary/list')
+            .then((response) =>{
+                console.log(response.data);
+            });
+    });
 
     return (
         <div>handleMontlyBase {id}</div>
